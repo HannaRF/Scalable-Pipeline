0b071848640bbbd54bcf8cb7cbea2a9c4f8ee67b
Rian Freitas da Silva <85463854+Rian-Freitas@users.noreply.github.com>
2024-07-03 03:17:24 -0300

    update status and total cost

--- update_quotes.py
+++ update_quotes.py
@@ -1,4 +1,5 @@
 import sqlite3
+import pandas as pd
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, lit
 from multiprocessing import Pool, Lock
@@ -17,14 +18,14 @@ def update_product(product, quantity_to_remove):
         connection.commit()
         connection.close()
 
-def update_status(quote_id, status):
+def update_status_and_total_cost(quote_id, status, total_cost):
     # Create a connection to the SQLite database
     connection = sqlite3.connect("fastdelivery/fastdelivery.db")
     cursor = connection.cursor()
 
     # Update the quote status
     with lock:
-        cursor.execute("UPDATE quote SET status = ? WHERE quote_id = ?;", (status, quote_id))
+        cursor.execute("UPDATE quote SET status = ?, total_cost = ? WHERE quote_id = ?;", (status, total_cost, quote_id))
         connection.commit()
         connection.close()
 
@@ -72,6 +73,9 @@ def update_quotes():
     # Get only consumer_id and neighborhood columns
     consumers = consumers.select("consumer_id", "neighborhood")
 
+    # rename the columns neighborhood to consumer_neighborhood
+    consumers = consumers.withColumnRenamed("neighborhood", "consumer_neighborhood")
+
     # filter the quotes that have status 'created'
     quotes = quotes.filter(col("status") == "created")
 
@@ -81,25 +85,26 @@ def update_quotes():
     # join the quotes with the products
     quotes = quotes.join(products, quotes.product_id == products.product_id)
 
-    # # join the quotes with the stores
-    # quotes = quotes.join(stores, quotes.store_id == stores.store_id)
+    # join the quotes with the stores
+    quotes = quotes.join(stores, quotes.store_id == stores.store_id)
 
-    # # join the quotes with the consumers
-    # quotes = quotes.join(consumers, quotes.consumer_id == consumers.consumer_id)
+    # join the quotes with the consumers
+    quotes = quotes.join(consumers, quotes.consumer_id == consumers.consumer_id)
 
-    # # calculate the total cost of the quote
-    # quotes = quotes.withColumn("total_cost", quotes.price * quotes.quantity + quotes.weight * quotes.weight_tax)
+    # calculate the total cost of the quote
+    quotes = quotes.withColumn("total_cost", quotes.price * quotes.quantity + quotes.quantity * quotes.weight / 1000 * quotes.weight_tax)
 
     quotes.show()
 
     # Collect the quote_ids and status
-    quote_ids = quotes.select("quote_id", "status").collect()
+    quote_ids = quotes.select("quote_id", "status", "total_cost").collect()
 
-    # Create a pool of workers
-    pool = Pool()
+    # put the quotes in a list
+    quote_ids = [quote for quote in quote_ids]
 
     # Update the quotes
     for quote in quote_ids:
-        pool.apply_async(update_status, args=(quote["quote_id"], quote["status"]))
+        update_status_and_total_cost(quote.quote_id, quote.status, quote.total_cost)
+
 
 update_quotes()
